f8764aeefa58ef87ba4f44d2a5c155c78d9c70ef
Phạm Hồng Hiệp <78995726+Epchannel@users.noreply.github.com>
2022-07-15 11:28:55 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -294,7 +294,7 @@ function reveal() {
   }
 
   var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "https://www.youtube.com/embed/Wu8NeFXaoOc?autoplay=1&mute=1");
+  ifrm.setAttribute("src", "https://www.youtube.com/embed/Wu8NeFXaoOc?autoplay=1");
   //ifrm.style.width = `${w}px`;
   //ifrm.style.height = `${h}px`;
   ifrm.style.border = 'none';
